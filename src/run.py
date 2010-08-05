bc92ad2c6e90567637bd955499ec1b056cebab7e
davide.malagoli1986 <davide.malagoli1986@c6aefd29-eeba-ca53-260b-65c6c3c55b4c>
2010-08-05 15:25:48 +0000

    magica working version

---
+++ src/run.py
@@ -0,0 +1,12 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="davide"
+__date__ ="$1-ago-2010 14.16.51$"
+
+from gts import *;
+from trucks import trucks_number
+from customer import customers
+
+if __name__ == "__main__":
+    print(gts(set(range(len(customers)))-set([0]),trucks_number(),10));

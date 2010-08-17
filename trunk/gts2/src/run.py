0a326405c06c6385406b19c74b6ebda0f0204678
davide.malagoli1986 <davide.malagoli1986@c6aefd29-eeba-ca53-260b-65c6c3c55b4c>
2010-08-17 13:17:23 +0000

    Fine di granular tabu search. Ora algoritmo genetico...

--- trunk/gts2/src/run.py
+++ trunk/gts2/src/run.py
@@ -10,4 +10,4 @@ from customer import customers
 
 
 if __name__ == "__main__":
-    print(taburoute(set(range(len(customers)))-set([0]),trucks_number(),50));
+    print(taburoute(set(range(len(customers)))-set([0]),trucks_number(),50,elapsed=0));

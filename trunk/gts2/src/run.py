c4393292f11ffcc9bae4a11ec50471390755e0e7
davide.malagoli1986@gmail.com <davide.malagoli1986@gmail.com@c6aefd29-eeba-ca53-260b-65c6c3c55b4c>
2010-08-19 14:28:17 +0000

    Preparato tutto per lanciare i comandi in remoto...

--- trunk/gts2/src/run.py
+++ trunk/gts2/src/run.py
@@ -8,6 +8,8 @@ from taburoute import *
 from trucks import trucks_number
 from customer import customers
 
+import sys
 
 if __name__ == "__main__":
-    print(taburoute(set(range(len(customers)))-set([0]),trucks_number(),50,elapsed=0));
+    print(sys.argv[1],sys.argv[2]);
+    print(taburoute(set(range(len(customers)))-set([0]),trucks_number(),50,lenght=int(sys.argv[1]),elapsed=int(sys.argv[2])));
---
+++ trunk/gts2/src/ssh.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+"""This runs 'ls -l' on a remote host using SSH. At the prompts enter hostname,
+user, and password.
+
+$Id: sshls.py 489 2007-11-28 23:40:34Z noah $
+"""
+
+import pexpect
+import getpass, os
+
+ssh_password=None;
+
+class SSHCommand:
+
+	def __init__(self,user, host, command):
+		self.user=user;
+		self.host=host;
+		if globals()['ssh_password']==None:
+			globals()['ssh_password']=getpass.getpass('Password: ');
+		self.password=globals()['ssh_password'];
+		self.command=command;
+
+	def launch(self):
+
+    		"""This runs a command on the remote host. This could also be done with the
+pxssh class, but this demonstrates what that class does at a simpler level.
+This returns a pexpect.spawn object. This handles the case when you try to
+connect to a new host and ssh asks you if you want to accept the public key
+fingerprint and continue connecting. """
+
+		user=self.user;
+		host=self.host;
+		password=self.password;
+		command=self.command;
+
+    		ssh_newkey = 'Are you sure you want to continue connecting';
+    		child = pexpect.spawn('ssh -l %s %s %s'%(user, host, command));
+    		i = child.expect([pexpect.TIMEOUT, ssh_newkey, 'password: ']);
+    		if i == 0: # Timeout
+        		print 'ERROR!';
+        		print 'SSH could not login. Here is what SSH said:';
+        		print child.before, child.after;
+        		return None;
+    		if i == 1: # SSH does not have the public key. Just accept it.
+        		child.sendline ('yes');
+        		child.expect ('password: ');
+        		i = child.expect([pexpect.TIMEOUT, 'password: ']);
+        		if i == 0: # Timeout
+            			print 'ERROR!';
+            			print 'SSH could not login. Here is what SSH said:';
+            			print child.before, child.after;
+            			return None;       
+    		child.sendline(password);
+		child.expect(pexpect.EOF);
+    		return child.before;
+
+	
+
+if __name__ == '__main__':
+    try:
+        #host = raw_input('Hostname: ')
+	#user = raw_input('User: ')
+	user='malagoli'
+	#password = getpass.getpass('Password: ')
+	
+	for host in ['ambrogio.cs.unibo.it','maria.cs.unibo.it']:
+		child = SSHCommand (user, host,'/bin/ls -l').launch();
+		print "\'{0}\'".format(child);
+	#print child.before
+    except Exception, e:
+        print str(e)
+        traceback.print_exc()
+        os._exit(1)
+

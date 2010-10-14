f91e04c097ece636db23e783ba5ce944182c6e9b
davide.malagoli1986@gmail.com <davide.malagoli1986@gmail.com@c6aefd29-eeba-ca53-260b-65c6c3c55b4c>
2010-10-14 09:24:39 +0000

    Pessimo il commit di prima...

--- trunk/gts2/src/genetic.py
+++ trunk/gts2/src/genetic.py
@@ -30,6 +30,8 @@ def initialize_population(range1,range2,n):
 
 	pop=[];
 #	for x in range(range1[0],range1[1]+1):
+#		for y in range(range2[0],range2[1]+1):
+#			pop+=[(x,y)];
 	for x in range(range1[0]):
 		for y in range(range2[0],range2[1]+1):
 			pop+=[(x,y)];
